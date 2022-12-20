be6e5829b25a2ee2586d1809294b9050cc0a073d
Jacob <shiersjake@gmail.com>
2022-12-20 17:55:46 -0600

    Added Java program to count  to 1 billion

---
+++ Java.java
@@ -0,0 +1,8 @@
+public class Java {
+    public static void main(String[] args) {
+        long count = 0;
+        for (long i = 0; i < 1000000000; i++) {
+            count++;
+        }
+    }
+}
