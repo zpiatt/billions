b4a95a58c154f5899f2c1fa2e687c1d5b1b158c7
zpiatt <zpiatt@protonmail.com>
2023-08-30 20:33:13 +0000

    remove comments from asm

--- billion.asm
+++ billion.asm
@@ -2,20 +2,16 @@
 .globl _start
 
 _start:
-    # Initialize i to 0 (use rdi register to store i)
     xor %rdi, %rdi
 
 loop:
-    # Check if i < 1000000000
     cmp $1000000000, %rdi
     jge end_loop
 
-    # Increment i by 1
     inc %rdi
     jmp loop
 
 end_loop:
-    # Exit syscall
     mov $60, %rax
     xor %rdi, %rdi
     syscall
